97064e715795f3164952e43289bc015d37364ad6
Aya Fetheddine <ayafetheddine@gmail.com>
2025-12-30 22:51:03 +0100

    add chat memory and system prompt to AIAgent

--- src/main/java/ma/fetheddine/emsibot/agents/AIAgent.java
+++ src/main/java/ma/fetheddine/emsibot/agents/AIAgent.java
@@ -3,14 +3,23 @@ package ma.fetheddine.emsibot.agents;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import reactor.core.publisher.Flux;
 
 @Component
 public class AIAgent {
     private ChatClient chatClient;
-
-    public AIAgent(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+    public AIAgent(ChatClient.Builder builder, ChatMemory memory) {
+        this.chatClient = builder
+                .defaultSystem("""
+                        Vous un assistant qui se charge de répondre aux question
+                        de l'utilisateur en fonction du contexte fourni.
+                        Si aucun contexte n'est fourni, répond avec JE NE SAIS PAS.
+                        """)
+                .defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(memory).build())
+                .build();
     }
 
     @GetMapping("/chat")
