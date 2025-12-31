c47274dbb1c34d44e279feb061081ffa57f71f21
Aya Fetheddine <ayafetheddine@gmail.com>
2025-12-31 03:19:13 +0100

    add agent tools from mcp server

--- mcp-server/src/main/java/ma/fetheddine/mcpserver/tools/McpTools.java
+++ mcp-server/src/main/java/ma/fetheddine/mcpserver/tools/McpTools.java
@@ -1,19 +1,19 @@
-package ma.fetheddine.emsibot.tools;
+package ma.fetheddine.mcpserver.tools;
 
-import org.springframework.ai.tool.annotation.Tool;
-import org.springframework.ai.tool.annotation.ToolParam;
+import org.springaicommunity.mcp.annotation.McpArg;
+import org.springaicommunity.mcp.annotation.McpTool;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-public class AITools {
-    @Tool(name = "getEmployee"
+public class McpTools {
+    @McpTool(name = "getEmployee"
             , description = "Get information about a given employee by name")
-    public Employee getEmployee(@ToolParam(description = "The employee name") String name){
+    public Employee getEmployee(@McpArg(description = "The employee name") String name){
         return new Employee(name, 15000.0, 5);
     }
-    @Tool(name = "getAllEmployees"
+    @McpTool(name = "getAllEmployees"
             , description = "Get information about all employees")
     public List<Employee> getAllEmployees(){
         return List.of(
